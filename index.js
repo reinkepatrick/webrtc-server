00c7a77c66469910c6c632d4e3cda7371dcb55e9
Patrick Reinke <patrick.reinke@fh-bielefeld.de>
2019-11-11 16:47:46 +0100

    add test socket connection

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const io = require('socket.io')();
 
-io.on('connection', client => {});
+io.on('connection', client => {
+    console.log(client);
+    client.on('disconnect', () => { console.log("Client disconneceted!") });
+});
 
 io.listen(3000);
