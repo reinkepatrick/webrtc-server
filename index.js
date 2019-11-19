d45c362cd009ecdbc6f704160ba03a15329bae44
Patrick Reinke <patrick.reinke@fh-bielefeld.de>
2019-11-19 09:23:30 +0100

    add DeviceList

--- index.js
+++ index.js
@@ -1,8 +1,39 @@
 const io = require('socket.io')();
 
+const getAllClients = () => {
+    let allClients = [];
+    Object.keys(clients).forEach((element, key, _array) => {
+        allClients = _array;
+    });
+
+    return allClients;
+}
+
+let clients = {};
+
 io.on('connection', client => {
-    console.log(client);
-    client.on('disconnect', () => { console.log("Client disconneceted!") });
+    client.on('ready', data => {
+        clients[client.id] = client;
+        let allClients = getAllClients();
+
+        io.emit('clients', {
+            clients: allClients
+        });
+    });
+
+    client.on('setDescription', (desc) => {
+        client.broadcast.emit('remoteDescription', desc);
+    });
+
+    client.on('disconnect', () => { 
+        console.log(client.id + ' disconnected!');
+        delete clients[client.id];
+        let allClients = getAllClients();
+
+        io.emit('clients', {
+            clients: allClients
+        });
+    });
 });
 
 io.listen(3000);
